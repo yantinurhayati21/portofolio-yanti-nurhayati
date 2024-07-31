a50d1d3d9bca6d3589499c655af9b9c553848f51
yanti <Yanti@LAPTOP-SGL1921R>
2024-07-31 22:15:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const texts = ["Junior Web Developer", "C# Developer", "Back-End Developer"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typing-text').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 2000);
+  } else {
+    setTimeout(type, 100); 
+  }
+})();
+
+(function erase() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, --index);
+
+  document.getElementById('typing-text').textContent = letter;
+  if (letter.length === 0) {
+    count++;
+    index = 0;
+    setTimeout(type, 500); // Delay before typing next word
+  } else {
+    setTimeout(erase, 50); // Erasing speed
+  }
+})();
